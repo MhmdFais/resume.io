ebc91e0bc05c5bb6b782bf1aecd2e0ea63d89b71
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-14 22:54:48 +0530

    chore: Remove unused email and phone fields in PersonalInfo component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -4,8 +4,6 @@ function PersonalInfo(){
 
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
     const [jobTiltle, setJobTitle] = useState('');
 
@@ -15,14 +13,6 @@ function PersonalInfo(){
 
     const handleLastNameChange = (event) => {
         setLastName(event.target.value);
-    }
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
-
-    const handlePhoneChange = (event) => {
-        setPhone(event.target.value);
     }
 
     const handleAddressChange = (event) => {
@@ -35,7 +25,7 @@ function PersonalInfo(){
 
     const submitHandle = (event) => {
         event.preventDefault();
-        console.log(firstName, lastName, email, phone, address, jobTiltle);
+        console.log(firstName, lastName, address, jobTiltle);
     }
 
     return (
@@ -49,17 +39,9 @@ function PersonalInfo(){
                     <label htmlFor="lastName">Last Name:</label>
                     <input type="text" id="lastName" value={lastName} onChange={handleLastNameChange} required/>
                 </div>
-                <div className="email">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" value={email} onChange={handleEmailChange} required/>
-                </div>
                 <div className="jobTitle">
                     <label htmlFor="jobTitle">Job Title:</label>
                     <input type="text" id="jobTitle" value={jobTiltle} onChange={handleJobTitleChange} required/>
-                </div>
-                <div className="phone">
-                    <label htmlFor="phone">Phone:</label>
-                    <input type="tel" id="phone" value={phone} onChange={handlePhoneChange} required/>
                 </div>
                 <div className="address">
                     <label htmlFor="address">Address:</label>
